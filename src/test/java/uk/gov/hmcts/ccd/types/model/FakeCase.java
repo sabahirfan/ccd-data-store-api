04d1d92253f13816c2d68d175006ca40cf3c1369
Alex McAusland <alex.mcausland@gmail.com>
2018-06-03 21:53:13 +0100

    Removing old case tab annotations

--- src/test/java/uk/gov/hmcts/ccd/types/model/FakeCase.java
+++ src/test/java/uk/gov/hmcts/ccd/types/model/FakeCase.java
@@ -5,21 +5,17 @@ import uk.gov.hmcts.ccd.ICase;
 import uk.gov.hmcts.ccd.definition.*;
 
 @NoArgsConstructor
-@CaseViewTabs(value = {"Core case", "Prosecutor", "Party"})
 public class FakeCase implements ICase {
 
     // Annotated fields are editable in the CCD UI.
     @CaseListField(label = "Defendant Name")
-    @CaseViewField(label = "Defendant name", tab = "Core case")
     @CaseSearchableField(label = "Defendant name", order = 2)
     private String defendantName;
 
     @CaseSearchableField(label = "Prosecutor name", order = 1)
     @CaseListField(label = "Prosecutor Name")
-    @CaseViewField(label = "Prosecutor name", tab = "Prosecutor")
     private String prosecutorName;
 
-    @CaseViewField(label = "Party", tab = "Party")
     @ComplexType
     private Party party = new Party();
 
