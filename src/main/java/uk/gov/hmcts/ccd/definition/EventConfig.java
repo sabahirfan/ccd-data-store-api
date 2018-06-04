143ac5e43fea18311175825d8a86f6861f72a77d
Rafal Kondratowicz <rkondr@gmail.com>
2018-06-04 21:48:39 +0100

    Add interface for events

--- src/main/java/uk/gov/hmcts/ccd/definition/EventConfig.java
+++ src/main/java/uk/gov/hmcts/ccd/definition/EventConfig.java
@@ -2,17 +2,13 @@ package uk.gov.hmcts.ccd.definition;
 
 import java.util.function.BiConsumer;
 
-public class EventConfig {
+public class EventConfig<T extends IEvent> {
 
-    public final Class objType;
-    public final BiConsumer<String, Object> handler;
+    public final Class<T> objType;
+    public final BiConsumer<String, T> handler;
 
-    public EventConfig(Class objType, BiConsumer<String, Object> handler) {
+    public EventConfig(Class<T> objType, BiConsumer<String, T> handler) {
         this.objType = objType;
         this.handler = handler;
-    }
-
-    public static EventConfig of(Class objType, BiConsumer<String, Object> handler) {
-        return new EventConfig(objType, handler);
     }
 }
