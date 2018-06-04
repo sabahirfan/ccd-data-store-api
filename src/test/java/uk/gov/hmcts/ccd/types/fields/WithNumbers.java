5bb2bdca93faee1c534db252cd63fbe6e5416c42
Alex McAusland <alex.mcausland@gmail.com>
2018-06-04 22:00:09 +0100

    bigint support

--- src/test/java/uk/gov/hmcts/ccd/types/fields/WithNumbers.java
+++ src/test/java/uk/gov/hmcts/ccd/types/fields/WithNumbers.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.ccd.types.fields;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.UUID;
 
 public class WithNumbers {
-    private int number;
-    private long another;
+    private int number = 1;
+    private long another = 1;
     private BigDecimal bigDecimal = BigDecimal.ONE;
+    private BigInteger bigInt = BigInteger.ONE.ONE;
 }
