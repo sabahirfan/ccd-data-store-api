accc60c271efb643ef70c2ea60f2f991863e6c4b
Alex McAusland <alex.mcausland@gmail.com>
2018-06-04 23:23:33 +0100

    Ignore empty optionals

---
+++ src/test/java/uk/gov/hmcts/ccd/types/fields/HasOptionals.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.ccd.types.fields;
+
+import org.assertj.core.util.Lists;
+
+import java.util.List;
+
+public class HasOptionals {
+    private List<HasOptional> list = Lists.newArrayList(new HasOptional());
+}
